56e5e5119fbe9812c4b71369991db7df57c9f101
danylboiko95 <44903844+danylboiko95@users.noreply.github.com>
2019-04-22 13:10:49 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ headers.append("Content-type", "application/json");
 
 const blocks = document.querySelectorAll('.block');
 const player = document.querySelector('#player');
+const spinner = document.querySelector('.spinner');
 
 const mark = `<div class="mark"></div>`
 const zero = `<div class="zero"></div>`
@@ -15,14 +16,92 @@ let id = 0;
 let name = '';
 
 
-document.querySelector('.btn').addEventListener('click', () => {
+document.querySelector('.btn').addEventListener('click', function initGame() {
 
 
     if (!player.value.match(/[a-z]/i)) {
-        alert('enter your name in english and press START GAME, please');
+        player.classList.add('player-wrong');
+        player.classList.add('shaking');
     } else {
+        
         name = player.value;
+        if (player.classList.contains('player-wrong')) {
+            player.classList.toggle('player-wrong');
+        }
+        player.classList.add('player-success');
+        document.querySelector('.btn').removeEventListener('click', initGame);
+        player.disabled = true;
         canPlay();
+        blocks.forEach(element => {
+
+            element.addEventListener('click', function startGame(e) {
+
+
+                name = player.value;
+                makeMove(element.dataset.number)
+                    .then(response => {
+                        return response.json();
+                    })
+                    .then(data => {
+                        if (data.ok) {
+                            
+                            if (!e.target.childNodes.length) {
+                                e.target.insertAdjacentHTML('afterbegin', mark);
+                                
+                            }
+                            if (!data.data.win) {
+                                
+                                return data.data.move;
+                            }
+                            else {//////////КОСТЫЛЬ!!!
+                                spinner.style.zIndex= '-111111';//нужна помощь в том, как прервать выполнение, что бы потом не добавлялся спиннер
+                                spinner.style.backgroundColor= 'white';
+
+                                ////КОСТЫЛЬ!!!
+                                player.classList.add('shaking');
+                                player.value = 'win';
+                                
+                            }
+                        }
+
+                    })
+                    .then(() => {
+                        spinner.style.display = 'block';
+                        waitMove()
+                            .then((response, reject) => {
+                                
+                                return response.json();
+                            })
+                            .then(data => {
+                                const dataMove = document.querySelector(`[data-number='${data.data.move}']`)
+
+
+                                if (!dataMove.childNodes.length) {//защита от дурака, если много раз клацать, на кнопку, то уже не появится много элементов
+                                    spinner.style.display = 'none';
+                                    const div = document.createElement('div');
+                                    div.classList.add('zero');
+                                    dataMove.appendChild(div);
+                                    
+                                }
+                                if (data.data.win === 0) {
+                                    spinner.style.display = 'none';
+                                    player.value = 'you lost';
+                                    if (!player.classList.contains('player-wrong')) {
+                                        player.classList.toggle('player-wrong');
+                                        
+                                    }
+                                    player.classList.add('shaking');
+                                }
+
+                            })
+                            .catch(reject => {
+                                console.log(reject);
+                            })
+                    })
+
+            })
+
+        })
     }
 })
 
@@ -31,63 +110,7 @@ document.querySelector('.btn').addEventListener('click', () => {
 
 
 
-blocks.forEach(element => {
 
-    element.addEventListener('click', function startGame(e) {
-
-        if (!name) {
-            alert('enter your name in english and press START GAME, please');
-        } else {
-            name = player.value;
-            makeMove(element.dataset.number)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    if (data.ok) {
-
-                        if (!e.target.childNodes.length) {
-                            e.target.insertAdjacentHTML('afterbegin', mark);
-                        }
-                        if (!data.data.win) {
-                            return data.data.move;
-                        }
-                        else {
-
-                            alert('win');
-                        }
-                    }
-
-                })
-                .then(() => {
-                    waitMove()
-                        .then(response => {
-                            return response.json();
-                        })
-                        .then(data => {
-                            const dataMove = document.querySelector(`[data-number='${data.data.move}']`)
-
-
-                            if (!dataMove.childNodes.length) {//защита от дурака, если много раз клацать, на кнопку, то уже не появится много элементов
-                                const div = document.createElement('div');
-                                div.classList.add('zero');
-                                dataMove.appendChild(div);
-
-                            }
-                            if (data.data.win === 0) {
-
-                                alert('you lost');
-                            }
-
-                        })
-                        .catch(error => {
-                            console.log(error);
-                        })
-                })
-        }
-    })
-
-})
 
 
 
@@ -109,6 +132,7 @@ function canPlay() {
 };
 
 function makeMove(move) {
+    
     return fetch(`${api}makeMove`, {
         method: 'POST',
         body: JSON.stringify({ move, id, name }),
@@ -117,6 +141,7 @@ function makeMove(move) {
 }
 
 function waitMove() {
+    
     return fetch(`${api}waitMove`, {
         method: "POST",
         body: JSON.stringify({ name, id }),
